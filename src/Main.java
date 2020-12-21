cc279d4d3cc9f3dbc3202497dad31d3b2daeea85
boog25 <voroshilov.d.v123@gmail.com>
2020-12-22 00:38:57 +0500

    gitignore - java

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 20;
+        int mili = 20;
+        int amount = price / mili;
+        System.out.println(amount);
+    }
+}
